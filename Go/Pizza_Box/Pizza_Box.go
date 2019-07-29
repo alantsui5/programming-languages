c64932eb799d5eaeee4bd2e5f4ff7fcd75e866cf
Ho Ming Tsui <4079tsui@gmail.com>
2019-07-29 23:45:44 +0800

    Finished Golang Pizza Box Project

--- Go/Pizza_Box/Pizza_Box.go
+++ Go/Pizza_Box/Pizza_Box.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type Pizza struct {
-	Name  string
-	Price float32
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
-func (p *Pizza) _SetName(name string) string {
-	if name != "" {
-		p.Name = name
-	}
-	return p.Name
+type Pizza struct {
+	Name   string
+	Price  float32
+	Number int
+	Cost   float32
 }
 
 func newPizza(name string, price float32) (*Pizza, error) {
@@ -22,15 +28,16 @@ func newPizza(name string, price float32) (*Pizza, error) {
 	}
 
 	return &Pizza{
-		Name:  name,
-		Price: price,
+		Name:   name,
+		Price:  price,
+		Number: 0,
+		Cost:   0,
 	}, nil
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+func (p *Pizza) getCost() float32 {
+	p.Cost = float32(p.Number) * p.Price
+	return p.Cost
 }
 
 func main() {
@@ -42,5 +49,53 @@ func main() {
 	check(err)
 	Pizzas["C"], err = newPizza("Pizza C", 34.11)
 	check(err)
+	var totalCost float32 = 0
 
+	for {
+		fmt.Println("\nBasket --------\n")
+
+		if Pizzas["A"].Number > 0 {
+			fmt.Printf("%s  %.2f\n", Pizzas["A"].Name, Pizzas["A"].getCost())
+		}
+		if Pizzas["B"].Number > 0 {
+			fmt.Printf("%s  %.2f\n", Pizzas["B"].Name, Pizzas["B"].getCost())
+		}
+		if Pizzas["C"].Number > 0 {
+			fmt.Printf("%s  %.2f\n", Pizzas["C"].Name, Pizzas["C"].getCost())
+		}
+		fmt.Printf("Total Cost %.2f\n---------------\n\n", totalCost)
+
+		fmt.Println("Please select a pizza to add to your basket:")
+		fmt.Printf("1: %s price: %.2f\n", Pizzas["A"].Name, Pizzas["A"].Price)
+		fmt.Printf("2: %s price: %.2f\n", Pizzas["B"].Name, Pizzas["B"].Price)
+		fmt.Printf("3: %s price: %.2f\n\n", Pizzas["C"].Name, Pizzas["C"].Price)
+
+		fmt.Println("Enter your choice here >> ")
+		choices, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		choices = strings.TrimSpace(choices)
+		check(err)
+		choice, err := strconv.Atoi(choices)
+		check(err)
+
+		switch choice {
+		case 1:
+			totalCost += Pizzas["A"].Price
+			Pizzas["A"].Number++
+			break
+		case 2:
+			totalCost += Pizzas["B"].Price
+			Pizzas["B"].Number++
+			break
+		case 3:
+			totalCost += Pizzas["C"].Price
+			Pizzas["C"].Number++
+			break
+		case 0:
+			fmt.Println("Terminating\n")
+			os.Exit(0)
+			break
+		default:
+			print("You inserted incorrect number")
+		}
+	}
 }
