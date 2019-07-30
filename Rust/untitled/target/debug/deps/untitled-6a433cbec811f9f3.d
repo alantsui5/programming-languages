d6eb06815a75e1203940deb9f95b4168aa2f8207
Alan Tsui <1155110323@link.cuhk.edu.hk>
2019-07-30 12:31:23 +0800

    Add rust files

--- Rust/untitled/target/debug/deps/untitled-6a433cbec811f9f3.d
+++
@@ -1,5 +0,0 @@
-/home/alan/Documents/programming-language/Rust/untitled/target/debug/deps/untitled-6a433cbec811f9f3: src/main.rs
-
-/home/alan/Documents/programming-language/Rust/untitled/target/debug/deps/untitled-6a433cbec811f9f3.d: src/main.rs
-
-src/main.rs:
--- Rust/untitled/target/debug/untitled.d
+++
@@ -1,1 +0,0 @@
-/home/alan/Documents/programming-language/Rust/untitled/target/debug/untitled: /home/alan/Documents/programming-language/Rust/untitled/src/main.rs
